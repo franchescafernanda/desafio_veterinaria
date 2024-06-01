dc73193b89e41853b269eb3c18ab81b816b02564
franchescafernanda <franchescafer26@gmail.com>
2024-05-31 23:09:25 -0400

    Adding project containing scripts, files and import

---
+++ backend/js/index.js
@@ -0,0 +1,13 @@
+const { registrar, leer } = require('./operaciones');
+
+const args = process.argv.slice(2);
+const operacion = args[0];
+
+if (operacion === "registrar") {
+    const [, nombre, edad, animal, color, enfermedad] = args;
+    registrar(nombre, edad, animal, color, enfermedad);
+} else if (operacion === "leer") {
+    leer();
+} else {
+    console.log("Operación no válida. Las operaciones válidas son 'registrar' y 'leer'.");
+}
---
+++ backend/js/operaciones.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const citasFilePath = './citas.json';
+
+function registrar(nombre, edad, animal, color, enfermedad) {
+    const nuevaCita = {
+        nombre,
+        edad,
+        animal,
+        color,
+        enfermedad
+    };
+
+    fs.readFile(citasFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error al leer el archivo de citas:", err);
+            return;
+        }
+
+        let citas = [];
+        if (data) {
+            citas = JSON.parse(data);
+        }
+
+        citas.push(nuevaCita);
+
+        fs.writeFile(citasFilePath, JSON.stringify(citas, null, 2), (err) => {
+            if (err) {
+                console.error("Error al escribir en el archivo de citas:", err);
+                return;
+            }
+            console.log("Cita registrada exitosamente.");
+        });
+    });
+}
+
+function leer() {
+    fs.readFile(citasFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error al leer el archivo de citas:", err);
+            return;
+        }
+
+        const citas = JSON.parse(data);
+        console.log("Citas registradas:");
+        citas.forEach((cita, index) => {
+            console.log(`Cita ${index + 1}:`);
+            console.log(`Nombre: ${cita.nombre}`);
+            console.log(`Edad: ${cita.edad}`);
+            console.log(`Tipo de animal: ${cita.animal}`);
+            console.log(`Color: ${cita.color}`);
+            console.log(`Enfermedad: ${cita.enfermedad}`);
+            console.log("----------------------");
+        });
+    });
+}
+
+module.exports = {
+    registrar,
+    leer
+};
